626d6c40a68e67ea213e80ecb7751c3cefb223e4
Sojib Roy <rcsojib.cse1@gmail.com>
2025-08-13 13:44:26 +0600

    Add files via upload

---
+++ 115-problems/Python Conditional Statements/12_Largest of Three Numbers.py
@@ -0,0 +1,17 @@
+'''
+12. Largest of Three Numbers: Write a Python program that takes three 
+numbers as input and prints the largest among them.
+'''
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+if a >= b and a >= c:
+    largest = a
+elif b >= a and b >= c:
+    largest = b
+else:
+    largest = c
+    
+print("The largest number is:", largest)
