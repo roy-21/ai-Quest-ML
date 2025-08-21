b49dcdfa828496bda14e2651ee4485ff8191bc8b
Sojib Roy <rcsojib.cse1@gmail.com>
2025-08-21 20:15:24 +0600

    Add files via upload

---
+++ 115-problems/Nested Loops/30_Multiplication Table.py
@@ -0,0 +1,9 @@
+# Program to print multiplication table from 1 to 10 using nested loops
+
+
+for i in range(1, 11):  
+    print(f"Multiplication table for {i}:")
+    for j in range(1, 11):  
+        result = i * j
+        print(f"{i} x {j} = {result}")
+    print()  
