05d4c36ede25926526e1bfc58c4603d0b921b1ee
Sojib Roy <rcsojib.cse1@gmail.com>
2025-08-25 00:08:29 +0600

    Add files via upload

---
+++ 115-problems/Tuple Problems/58_Tuple Packing.py
@@ -0,0 +1,13 @@
+'''
+Tuple Packing: Write a Python program to pack three variables into a 
+single tuple and print the tuple.
+'''
+
+#input
+a = int(input("Enter first value: "))
+b = int(input("Enter second value: "))
+c = int(input("Enter third value: "))
+
+#tuple packing
+t = (a, b, c)
+print("Packed tuple:", t)
