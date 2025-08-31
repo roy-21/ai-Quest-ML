b0a984515648ccc84ea393203ff8b418b528ee43
Sojib Roy <rcsojib.cse1@gmail.com>
2025-08-31 20:31:54 +0600

    Add files via upload

---
+++ 115-problems/Break & Continue/98_Positive Number Sum.py
@@ -0,0 +1,16 @@
+'''
+Positive Number Sum: Write a Python program that takes positive numbers as 
+input until a negative number is entered. Then, calculate and print the sum 
+of all positive numbers entered. Use a `while` loop and `break` to exit 
+the loop when a negative number is encountered.
+'''
+
+total = 0
+
+while True:  
+    num = int(input("Enter a number (negative to stop): "))
+    if num < 0:    
+        break
+    total += num      
+
+print("Sum of positive numbers:", total)
